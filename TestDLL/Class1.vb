a45e1ea84369ef6a01c547123a81ca034923c2db
daddylongtoe <steve.mcloughlin@hotmail.co.uk>
2017-12-12 15:20:46 +0000

    Added test files

---
+++ TestDLL/Class1.vb
@@ -0,0 +1,11 @@
+﻿Namespace JDRAutomation
+    Public Class MyFunctions
+
+        Public Function Add(ByVal NumberOne As Integer, ByVal NumberTwo As Integer) As Integer
+
+            Return NumberOne + NumberTwo
+
+        End Function
+
+    End Class
+End Namespace
